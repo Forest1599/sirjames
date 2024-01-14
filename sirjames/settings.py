a3111b70066df96582568a1deb1970ba74e703ca
Eriks Benne <eriksbenne99@gmail.com>
2024-01-14 14:56:01 +0000

    Added animations to sections

--- sirjames/settings.py
+++ sirjames/settings.py
@@ -135,6 +135,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'jamesmabote123@gmail.com'
 
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST_PASSWORD = "lmenxpztkojjvbet"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
