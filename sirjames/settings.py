afafecbcd4fe301eea24df60b2f98cac48e5dec0
Eriks Benne <eriksbenne99@gmail.com>
2024-01-14 14:17:52 +0000

    Reviews added to the website

--- sirjames/settings.py
+++ sirjames/settings.py
@@ -135,7 +135,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'jamesmabote123@gmail.com'
 
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-# EMAIL_HOST_PASSWORD = 'lmenxpztkojjvbet'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
